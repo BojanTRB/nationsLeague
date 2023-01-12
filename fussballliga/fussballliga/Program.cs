90b93066537aa9754b210288f0405aaeb0712e43
Bojan <trb.bojan.privat@gmail.com>
2023-01-12 08:51:36 +0100

    Created .net console application

---
+++ fussballliga/fussballliga/Program.cs
@@ -0,0 +1,15 @@
+﻿using fussballliga.Models;
+
+using var db = new SoccerLeagueContext();
+
+
+
+Console.WriteLine("Gib den Namen der League ein:");
+do
+{
+    string name = Console.ReadLine();
+}
+while (name != null);
+
+db.Leagues.Add(new League() { Name = name, CreatedOn = DateTime.UtcNow });
+db.SaveChanges();
