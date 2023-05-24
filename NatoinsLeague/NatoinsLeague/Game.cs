254ca680d4d271bd5f77fc2686c8c0bf4cd33c72
Bojan <trb.bojan.privat@gmail.com>
2023-05-24 12:25:46 +0200

    Created Solution

---
+++ NatoinsLeague/NatoinsLeague/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatoinsLeague
+{
+    public class Game
+    {
+        public record GameR(
+            [property: Column("G_ID")] int Id,
+            [property: Column("G_HomeTeam")] int HomeId,
+            [property: Column("G_AwayTeam")] int AwayId,
+            [property: Column("G_DateTime")] GameDate GameDat
+            );
+    }
+}
---
+++ NatoinsLeague/NatoinsLeague/GameDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatoinsLeague
+{
+    public class GameDate
+    {
+        public int? GameDay;
+        public DateTime? GameDat;
+
+        public GameDate(int gameDay, DateTime gameDat)
+        {
+            GameDay = gameDay;
+            GameDat = gameDat;
+        }
+    }
+}
